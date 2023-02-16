67590283ba5e7835641311e6226ffe2f00a48b94
fmadala@hotmail.com <fmadala@hotmail.com>
2023-02-16 13:31:54 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main-this program prints a line using puts function
  * return: =0
  */
-int main(void)
+int main(void) /* main arguments=0 */
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
