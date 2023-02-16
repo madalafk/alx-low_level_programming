e3597c79110810d20cdf751d7c1e8bec67f20c4c
fmadala@hotmail.com <fmadala@hotmail.com>
2023-02-16 13:03:07 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-this program prints a line using puts function
+ * return: =0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+/* return is 0 */
