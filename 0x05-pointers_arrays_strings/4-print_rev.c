523086590e303cb739837a5e6f7c5cace6adc3d9
fmadala@hotmail.com <fmadala@hotmail.com>
2023-02-27 10:06:23 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - ptints string in reverse
+ * @s: string
+ * return: Always 0 (success)
+ */
+void print_rev(char *s)
+{
+
+	int longint = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longint++;
+		s++;
+	}
+	s--;
+	for (o = longint; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+}
