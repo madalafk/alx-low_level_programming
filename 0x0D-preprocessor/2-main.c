417edee5b7eb8dc362af1dab2aa6af48452e017c
fmadala@hotmail.com <fmadala@hotmail.com>
2023-03-20 10:43:31 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the file name8 it was compiled from
+ *
+ * Return: Always 0.(Success)
+ */
+
+int main(void)
+
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
