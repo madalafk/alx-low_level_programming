7f9f0eb58175560580e959fb564c19d42a9f4986
fmadala@hotmail.com <fmadala@hotmail.com>
2023-03-23 10:31:16 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,21 +1,12 @@
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
-#include <stdlib.h>
 #include <stddef.h>
+#include <stdlib.h>
 
-
-/**
-* int_index -  returns the index of the first element if cmp = 0
-* _putchar - writes the character c to stdout
-* @c: The character to print
-* Return: On success 1.
-*
-*/
-
-int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int)); ls
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
