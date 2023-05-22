7ee58954f5f59a123ad103b7efa53f504a35598d
fmadala@hotmail.com <fmadala@hotmail.com>
2023-05-22 11:59:28 -0700

    isupper

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if c is upper or lower
+ * @c: the input character provided
+ *
+ * Description: this  check for uppercase letters based on the input
+ * Return: 0 on success
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
